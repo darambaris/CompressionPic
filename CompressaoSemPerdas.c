d7b6dfb5924fd19b392689fab3cfd3457aa20d34
Jessika Darambaris <jessika.darambaris@gmail.com>
2015-09-21 19:21:07 -0300

    enviando tudo

---
+++ CompressaoSemPerdas.c
@@ -0,0 +1,11 @@
+/*
+ ============================================================================================================
+ Nome         : CompressaoComPerdas.c
+ Autores      : Jéssika Darambaris e Roberto Freitas
+ Descrição    : Arquivo responsável por implementar a técnica de compressão transformada dos cossenos (DCT)
+ ============================================================================================================
+ */
+
+int compressaoSemPerdas(){
+	return EXIT_SUCCESS;	
+}
---
+++ bmp.h
@@ -0,0 +1,34 @@
+#ifndef _BMP_H_
+#define _BMP_H_
+
+/**** BMP file header structure ****/
+typedef struct {
+    unsigned short bfType;       /* Magic number for file */
+} BMPMAGICNUMBER;
+
+typedef struct {
+    unsigned int   bfSize;       /* Size of file */
+    unsigned short bfReserved1;  /* Reserved */
+    unsigned short bfReserved2;  /* ... */
+    unsigned int   bfOffBits;    /* Offset to bitmap data */
+} BMPFILEHEADER;
+
+/* bfType deve ser = "MB" */
+
+/**** BMP file info structure ****/
+typedef struct {
+    unsigned int   biSize;          /* Size of info header */
+    int            biWidth;         /* Width of image */
+    int            biHeight;        /* Height of image */
+    unsigned short biPlanes;        /* Number of color planes */
+    unsigned short biBitCount;      /* Number of bits per pixel */
+    unsigned int   biCompression;   /* Type of compression to use */
+    unsigned int   biSizeImage;     /* Size of image data */
+    int            biXPelsPerMeter; /* X pixels per meter */
+    int            biYPelsPerMeter; /* Y pixels per meter */
+    unsigned int   biClrUsed;       /* Number of colors used */
+    unsigned int   biClrImportant;  /* Number of important colors */
+} BMPINFOHEADER;
+
+
+#endif
